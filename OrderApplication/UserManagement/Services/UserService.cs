afcd388e307b2d37a3b8b606af99e3feb644c3b5
meenakshithukral <meenakshi.thukral@nagarro.com>
2020-03-29 18:19:54 +0530

    added code

---
+++ OrderApplication/UserManagement/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserManagement.Models;
+using UserManagement.Repository;
+
+namespace UserManagement.Services
+{
+    public class UserService : IUserService
+    {
+        public User getUserDetails(int Id)
+        {
+            return UserRepository.GetUserDetails(Id);
+        }
+    }
+}
